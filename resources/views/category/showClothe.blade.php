05fdc88142e4466b53fb8b9f131f9c4615a0180c
Sebastain Velez <svelezg4@eafit.edu.co>
2022-09-20 10:01:55 -0500

    changed some views, sneakers, accessories and clothes

--- resources/views/category/showClothe.blade.php
+++ resources/views/category/showClothe.blade.php
@@ -36,51 +36,6 @@
       </button>
     </div>
 
-    <h3 class="text-center my-5">Highlights</h3>
-
-    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-          aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-      <div class="carousel-inner">
-        @for ($i = 0; $i < min(count($viewData['clothes']), 6); $i += 2)
-          <div class="carousel-item {{ $i == 0 ? 'active' : '' }}">
-            @if ($i + 1 < count($viewData['clothes']))
-              <div class="d-flex">
-                <a href="{{ route('clothes.index', $viewData['clothes'][$i]->getId()) }}">
-                  <img
-                    src="{{ URL::to('/') }}/image/clothes/{{ $viewData['clothes'][$i]->getId() }}/{{ $viewData['clothes'][$i]->getImage() }}"
-                    style="height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
-                </a>
-                <a href="{{ route('clothes.index', $viewData['clothes'][$i + 1]->getId()) }}">
-                  <img
-                    src="{{ URL::to('/') }}/image/clothes/{{ $viewData['clothes'][$i + 1]->getId() }}/{{ $viewData['clothes'][$i + 1]->getImage() }}"
-                    style="height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
-                </a>
-              </div>
-            @else
-              <a href="{{ route('clothes.index', $viewData['clothes'][$i]->getId()) }}">
-                <img
-                  src="{{ URL::to('/') }}/image/clothes/{{ $viewData['clothes'][$i]->getId() }}/{{ $viewData['clothes'][$i]->getImage() }}"
-                  style="width: 100%; height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
-              </a>
-            @endif
-          </div>
-        @endfor
-      </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
-    </div>
-
     <div class="row">
       <h2 class="text-center my-5">{{ $viewData['category']->getName() }}</h2>
 
