9ff5b1f28df58c2d04b7a99259f12ea131ee169e
unknown <npellegrib@eafit.edu.co>
2022-11-13 17:24:57 -0500

    lang

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,22 +19,28 @@
               </button>
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                   <div class="navbar-nav ms-auto">
-                      <a class="btn btn-outline-success mx-2" href="{{ route('home.index')}}">Home</a>
-                      <a class="btn btn-outline-success mx-2" href="{{ route('category.index')}}">Categories</a>
-                      <a class="btn btn-outline-success mx-2" href="{{ route('home.about')}}">About</a>
-                      <a class="btn btn-outline-success mx-2" href="{{ route('home.support')}}">Support</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.index')}}">Home</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('category.index')}}">Categories</a>
+
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">Sneakers</a>
+
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">Clothing</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.support')}}">Accessories</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">About</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.support')}}">Support</a>
                       <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+
                       @guest
-                          <a class="btn btn-outline-success mx-2" href="{{ route('login') }}">Login</a>
-                          <a class="btn btn-outline-success mx-2" href="{{ route('register') }}">Register</a>
+                          <a class="btn btn-outline-light mx-2" href="{{ route('login') }}">Login</a>
+                          <a class="btn btn-outline-light mx-2" href="{{ route('register') }}">Register</a>
                       @else
                       @if (Auth::user()->getRol() == 'admin')
                           <li class="nav-item d-flex">
-                              <a class="btn btn-outline-success mx-2" href="{{ route('admin.index', Auth::id()) }}">Admin Panel</a>
+                              <a class="btn btn-outline-light mx-2" href="{{ route('admin.index', Auth::id()) }}">Admin Panel</a>
                           </li>
                       @endif
                           <form id="logout" action="{{ route('logout') }}" method="POST">
-                              <a role="button" class="btn btn-outline-success mx-2"
+                              <a role="button" class="btn btn-outline-light mx-2"
                               onclick="document.getElementById('logout').submit();">Logout</a>
                               @csrf
                           </form>
@@ -46,7 +52,7 @@
         <!-- nav -->
 
         <!-- header -->
-        <header class="masthead bg-secondary text-white text-center py-4">
+        <header class="masthead bg-black text-white text-center py-4">
             <div class="container d-flex align-items-center flex-column">
                 <h2>@yield('subtitle', 'A Laravel SNEAKERS App')</h2>
             </div>
@@ -58,7 +64,7 @@
         </div>
         <!-- footer -->
 
-    <footer class="bg-secondary text-center text-white absolute-bottom">
+    <footer class="bg-dark text-center text-white absolute-bottom">
         <div class="container p-4">
             <section class="mb-4">
                 <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fas fa-fw fa-facebook"></i></a>
@@ -74,15 +80,14 @@
                 </p>
             </section>
         </div>
-    <div class="text-center bg-primary p-3" style="">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
       © 2022 Copyright:
       <div class="d-flex flex-column">
         <a class="text-white" href="https://mdbootstrap.com/">Juan Esteban Cardona</a>
         <a class="text-white" href="https://mdbootstrap.com/">Sebastian Velez</a>
-        <a class="text-white" href="https://mdbootstrap.com/">Neller Pellegrino</a>
       </div>
     </div>
-
+c
   </footer>
   <!-- footer -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
---
+++ resources/views/sneaker/index.blade.php
@@ -0,0 +1,102 @@
+@extends("layouts.app")
+@section('title', $viewData['title'])
+@section('content')
+
+
+<div class="bg-dark text-light pt-5">
+  <div class="container">
+    <div class="row d-flex justify-content-center align-items-center" style="min-height: calc(40vh - 3rem)">
+      <div class="col-md-6">
+        <h1 class="text-center">{{ $viewData['sneaker']->getName()}} {{ $viewData['sneaker']->getColorway() }}</h1>
+
+          <ul class="list-group list-group-flush rounded">
+            <li class="list-group-item d-flex bg-dark text-light">
+              <p class="w-100">
+                <i class="fas fa-user"></i>
+                <span class="font-weight-bold">Name: </span>
+                {{ $viewData['sneaker']->getName() }}
+              </p>
+              <p class="w-100">
+                <i class="fas fa-tag"></i>
+                <span class="font-weight-bold">Brand: </span>
+                {{ $viewData['sneaker']->getBrand() }}
+              </p>
+            </li>
+            <li class="list-group-item d-flex bg-dark text-light">
+              <p class="w-100">
+                <i class="fas fa-dollar-sign"></i>
+                <span class="font-weight-bold">Colorway: </span>
+                {{ $viewData['sneaker']->getColorway() }}
+              </p>
+              <p class="w-100">
+                <i class="fas fa-id-badge"></i>
+                <span class="font-weight-bold">Release Date: </span>
+                {{ $viewData['sneaker']->getReleasedate() }}
+              </p>
+            </li>
+            <li class="list-group-item d-flex bg-dark text-light">
+              <p class="w-100">
+                <i class="fas fa-abacus"></i>
+                <span class="font-weight-bold">Retail Price: </span>
+                {{ $viewData['sneaker']->getRetailprice() }}
+              </p>
+              <p class="w-100">
+                <i class="fas fa-calendar-plus"></i>
+                <span class="font-weight-bold">Price: </span>
+                {{ $viewData['sneaker']->getprice() }}
+              </p>
+            </li>
+            <li class="list-group-item d-flex bg-dark text-light">
+              <p class="w-100">
+                <i class="fas fa-calendar"></i>
+                <span class="font-weight-bold">Description: </span>
+                {{ $viewData['sneaker']->getDescription() }}
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <img
+            src="{{ URL::to('/') }}/image/sneakers/{{ $viewData['sneaker']->getId() }}/{{ $viewData['sneaker']->getImage() }}"
+            class="card-img-top" alt="..." style="max-height: 80vh; object-fit:cover;" ; />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="carousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      @for ($i = 0; $i < count($viewData['images']); $i += 2)
+        <div class="carousel-item {{ $i == 0 ? 'active' : '' }}">
+          @if ($i + 1 < count($viewData['images']))
+            <div class="d-flex">
+              <img
+                src="{{ URL::to('/') }}/image/sneakers/{{ $viewData['sneaker']->getId() }}/{{ $viewData['images'][$i]->getFilename() }}"
+                style="height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
+              <img
+                src="{{ URL::to('/') }}/image/sneakers/{{ $viewData['sneaker']->getId() }}/{{ $viewData['images'][$i + 1]->getFilename() }}"
+                style="height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
+            </div>
+          @else
+            <img
+              src="{{ URL::to('/') }}/image/sneakers/{{ $viewData['sneaker']->getId() }}/{{ $viewData['images'][$i]->getFilename() }}"
+              style="width: 100%; height: 50vh; object-fit: cover" class="d-block w-100" alt="..." />
+          @endif
+        </div>
+      @endfor
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  @endsection
