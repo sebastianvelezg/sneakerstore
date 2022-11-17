213661770173cf4064a32777a59d7b72b66ea0d8
Neller Pellegrino Baquero <68928357+npellegrib@users.noreply.github.com>
2022-11-16 22:24:40 -0500

    lang (#12)

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+        <title>@yield('title', 'Sneakers store')</title>
+
+    </head>
+    <body>
+        <!-- nav -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-third py-4">
+          <div class="container">
+              <a class="navbar-brand" href="{{ route('home.index') }}">Sneakers store</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav ms-auto">
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.index')}}">Home</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('category.index')}}">Categories</a>
+
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">Sneakers</a>
+
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">Clothing</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.support')}}">Accessories</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.about')}}">About</a>
+                      <a class="btn btn-outline-light mx-2" href="{{ route('home.support')}}">Support</a>
+                      <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+
+                      @guest
+                          <a class="btn btn-outline-light mx-2" href="{{ route('login') }}">Login</a>
+                          <a class="btn btn-outline-light mx-2" href="{{ route('register') }}">Register</a>
+                      @else
+                      @if (Auth::user()->getRol() == 'admin')
+                          <li class="nav-item d-flex">
+                              <a class="btn btn-outline-light mx-2" href="{{ route('admin.index', Auth::id()) }}">Admin Panel</a>
+                          </li>
+                      @endif
+                          <form id="logout" action="{{ route('logout') }}" method="POST">
+                              <a role="button" class="btn btn-outline-light mx-2"
+                              onclick="document.getElementById('logout').submit();">Logout</a>
+                              @csrf
+                          </form>
+                      @endguest
+                  </div>
+              </div>
+          </div>
+      </nav>
+        <!-- nav -->
+
+        <!-- header -->
+        <header class="masthead bg-black text-white text-center py-4">
+            <div class="container d-flex align-items-center flex-column">
+                <h2>@yield('subtitle', 'A Laravel SNEAKERS App')</h2>
+            </div>
+        </header>
+        <!-- header -->
+        
+        <div class="container my-4">
+            @yield('content')
+        </div>
+        <!-- footer -->
+
+    <footer class="bg-dark text-center text-white absolute-bottom">
+        <div class="container p-4">
+            <section class="mb-4">
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fas fa-fw fa-facebook"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
+            </section>
+            <section class="mb-4">
+                <p>
+                @lang('description')
+                </p>
+            </section>
+        </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      © 2022 Copyright:
+      <div class="d-flex flex-column">
+        <a class="text-white" href="https://mdbootstrap.com/">Juan Esteban Cardona</a>
+        <a class="text-white" href="https://mdbootstrap.com/">Sebastian Velez</a>
+      </div>
+    </div>
+c
+  </footer>
+  <!-- footer -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+  </body>
+
+</html>
---
+++ resources/views/sneaker/indexAdmin.blade.php
@@ -0,0 +1,69 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+@section('content_header')
+<h1>Sneakers List</h1>
+@stop
+@section('content')
+
+    <div class="d-flex flex-wrap justify-content-around">
+      @foreach ($viewData['categories'] as $category)
+      <div class="card" style="width: 40%;">
+        <div class="card-header">
+          <h3 class="card-title">{{ $category->getName() }}</h3>
+          <div class="card-tools">
+            <a href="{{ route('admin.sneakerCreate', ['id'=> $category->getId()]) }}" class="btn btn-dark mb-3">
+            Create
+              <i class="fas fa-plus"></i>
+            </a>
+            <a href="{{ route('admin.sneakersCategory', $category->getId()) }}" class="btn btn-dark mb-3">
+            See All
+              <i class="fas fa-eye"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    @endforeach
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+{{-- @foreach($sneakers as $sneaker)
+  <div class="card" style="width: 40%;">
+    <div class="card-header">
+      <h3 class="card-title">{{ $sneaker->getName() }} &nbsp;</h3>
+      <h3 class="card-title">{{ $sneaker->getColorway() }} </h3>
+      <form class="card-tools" action="{{ route('admin.sneakerDelete', $sneaker->getId()) }}">
+        @csrf @method('GET')
+        <a href="{{ route('admin.sneakerEdit', $sneaker->getId()) }}" class="btn btn-dark mb-3">
+        Edit
+          <i class="fas fa-pen"></i>
+        </a>
+        <button class="btn btn-dark mb-3" type="submit">
+        Delete
+          <i class="fas fa-trash"></i>
+        </button>
+      </form>
+    </div>
+    <div class="card-body d-flex justify-content-around">
+      
+      <p style="display: -webkit-box; -webkit-line-clamp: 6; -webkit-box-orient: vertical; overflow: hidden;">
+        {{ $sneaker->getDescription() }}</p>
+    </div>
+    <div class="card-footer d-flex justify-content-around">
+      <i class="fas fa-calendar"></i>
+      <p>{{ $sneaker->getCreateAt() }}</p>
+      <p>{{ $sneaker->getUpdateAt() }}</p>
+      <i class="fas fa-calendar"></i>
+    </div>
+  </div>
+@endforeach --}}
+
+{{-- <img src="./../../../image/sneaker/{{ $sneaker->getImage() }}" alt="" width="200" class="mx-5"
+        style="max-height: 10rem;"> --}}
